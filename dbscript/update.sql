ba28c952f8fa967d347c91767d0a2d0e011feb79
shirne <shirne@126.com>
2018-05-11 17:07:55 +0800

    fix

--- dbscript/update.sql
+++ dbscript/update.sql
@@ -1,13 +1,13 @@
 update `sa_permission` set icon=replace(icon,'ion-','ion-md-') where id>0;
 
-update `sa_permission` set icon='ion-md-apps' where 'key'='Content';
-update `sa_permission` set icon='ion-md-cog' where 'key'='System';
-update `sa_permission` set icon='ion-md-medical' where 'key'='category_index';
-update `sa_permission` set icon='ion-md-paper' where 'key'='article_index';
-update `sa_permission` set icon='ion-md-megaphone' where 'key'='notice_index';
-update `sa_permission` set icon='ion-md-chatbubbles' where 'key'='feedback_index';
-update `sa_permission` set icon='ion-md-people' where 'key'='member_level_index';
-update `sa_permission` set icon='ion-md-options' where 'key'='setting_index';
+update `sa_permission` set icon='ion-md-apps' where `key`='Content';
+update `sa_permission` set icon='ion-md-cog' where `key`='System';
+update `sa_permission` set icon='ion-md-medical' where `key`='category_index';
+update `sa_permission` set icon='ion-md-paper' where `key`='article_index';
+update `sa_permission` set icon='ion-md-megaphone' where `key`='notice_index';
+update `sa_permission` set icon='ion-md-chatbubbles' where `key`='feedback_index';
+update `sa_permission` set icon='ion-md-people' where `key`='member_level_index';
+update `sa_permission` set icon='ion-md-options' where `key`='setting_index';
 
 ALTER TABLE `sa_article` ADD `digg` INT(11) DEFAULT '0',ADD `comment` INT(11) DEFAULT '0',ADD `views` INT(11) DEFAULT '0';
 
