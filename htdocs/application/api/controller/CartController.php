b2e845696bfab6a5dfe11c37448e90abc3afa754
shirne <shirne@126.com>
2018-11-20 17:45:49 +0800

    购物车 API更新

--- htdocs/application/api/controller/CartController.php
+++ htdocs/application/api/controller/CartController.php
@@ -2,6 +2,9 @@
 
 namespace app\api\Controller;
 
+use app\common\facade\MemberCartFacade;
+use think\Db;
+
 /**
  * 购物车操作接口
  * Class CartController
@@ -10,22 +13,45 @@ namespace app\api\Controller;
 class CartController extends AuthedController
 {
     public function getall(){
-
+        return $this->response(MemberCartFacade::getCart($this->user['id']));
     }
 
-    public function add(){
-
+    public function add($sku_id,$count=1){
+        $sku=Db::name('ProductSku')->where('sku_id',$sku_id)->find();
+        if(empty($sku)){
+            $this->error('型号不存在');
+        }
+        if($sku['storage']<1){
+            $this->error('型号库存不足');
+        }
+        $product=Db::name('Product')->where('id',$sku['product_id'])
+            ->where('status',1)->find();
+        if(empty($product)){
+            $this->error('产品已下架');
+        }
+        MemberCartFacade::addCart($product,$sku_id,$count,$this->user['id']);
+        $this->success('成功添加到购物车');
     }
 
-    public function update(){
-
+    public function update($sku_id,$count=1){
+        $sku=Db::name('ProductSku')->where('sku_id',$sku_id)->find();
+        if(empty($sku)){
+            $this->error('型号不存在');
+        }
+        if($count>$sku['storage']){
+            $count=$sku['storage'];
+        }
+        MemberCartFacade::updateCart($sku_id,$count,$this->user['id']);
+        $this->success('购物车已更新');
     }
 
-    public function delete(){
-
+    public function delete($sku_id){
+        MemberCartFacade::delCart($sku_id,$this->user['id']);
+        $this->success('购物车已更新');
     }
 
     public function clear(){
-
+        MemberCartFacade::clearCart($this->user['id']);
+        $this->success('购物车已清空');
     }
 }
