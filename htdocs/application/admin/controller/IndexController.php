2a3f4345b80eeef0577208b94e0bc746055ae100
shirne <shirne@126.com>
2019-01-04 13:39:37 +0800

    清空会员数据优化

--- htdocs/application/admin/controller/IndexController.php
+++ htdocs/application/admin/controller/IndexController.php
@@ -94,33 +94,29 @@ class IndexController extends BaseController{
      * 清空测试数据
      */
     public function clear(){
-        $sqls='truncate table `sa_member`;
-            truncate table `sa_member_address`;
-            truncate table `sa_member_cashin`;
-            truncate table `sa_member_log`;
-            truncate table `sa_member_money_log`;
-            truncate table `sa_member_recharge`;
-            truncate table `sa_member_oauth`;
-            truncate table `sa_member_cart`;
-            truncate table `sa_member_card`;
-            truncate table `sa_invite_code`;
-            truncate table `sa_checkcode`;
-            
-            truncate table `sa_order`;
-            truncate table `sa_order_product`;
-            truncate table `sa_subscribe`;
-            truncate table `sa_feedback`;
-            truncate table `sa_manager_log`;';
-        foreach (explode(';',$sqls) as $sql){
-            $sql=trim($sql);
-            if(empty($sql))continue;
-            Db::execute($sql);
+
+        $tables=['member','checkcode','checkcode_limit'];
+        foreach ($tables as $table) {
+            Db::query('TRUNCATE TABLE `'.config('database.prefix').$table.'`');
         }
+
+        $tables=Db::query('show tables');
+        $field = 'Tables_in_'.config('database.database');
+
+        foreach ($tables as $row){
+            $columns=Db::query('show columns in '.$row[$field]);
+            $fields=array_column($columns,'Field');
+            if(in_array('member_id',$fields)
+            || in_array('order_id',$fields)){
+                Db::execute('TRUNCATE TABLE `'.$row[$field].'`');
+            }
+        }
+
         @unlink('./uploads/qrcode');
         @unlink('./uploads/avatar');
         user_log($this->mid,'cleardata',1,'清空会员数据','manager');
         
-        $this->success('数据已清空');
+        $this->success('数据已清空',url('index/index'));
     }
 
     /**
