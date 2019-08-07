42e063e9c83b1d466009c9473499ff0bd993eba9
shirne <shirne@126.com>
2019-08-08 07:36:21 +0800

    更新thinkphp版本

--- htdocs/bak.sh
+++ htdocs/bak.sh
@@ -9,7 +9,7 @@ a=`date +%Y%m%d%H%M%S`
 
 if [ "$2" = "sql" ];then
 	cd /data/bak/
-	mysqldump -u ycyclub -p123456 $1>/data/bak/$1-$a.sql
+	mysqldump -u $1 -p123456 $1>/data/bak/$1-$a.sql
 	tar cfz $1-$a.sql.tar.gz $1-$a.sql
 fi
 
