cffed6168599e8138b62357cc7bb0b226ae610be
shirne <shirne@126.com>
2019-10-17 04:02:15 +0800

    成本价数据库默认值

--- dbscript/updates/20191012.sql
+++ dbscript/updates/20191012.sql
@@ -1,9 +1,9 @@
 
 ALTER TABLE `sa_order_product` 
-ADD `product_cost_price` DECIMAL(10,2) DEFAULT NULL AFTER `product_price`;
+ADD `product_cost_price` DECIMAL(10,2) DEFAULT 0 AFTER `product_price`;
 
 ALTER TABLE `sa_order` 
-ADD `cost_amount` DECIMAL(10,2) DEFAULT NULL AFTER `product_amount`;
+ADD `cost_amount` DECIMAL(10,2) DEFAULT 0 AFTER `product_amount`;
 
 ALTER TABLE `sa_order` 
 ADD UNIQUE INDEX `order_no_unique` (`order_no` ASC);
