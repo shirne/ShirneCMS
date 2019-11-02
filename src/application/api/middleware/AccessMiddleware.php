81906ff6095ebc8033d42c86308c6c89679b3376
shirne <shirne@126.com>
2019-11-02 22:19:34 +0800

    API针对OPTIONS请求的处理

--- src/application/api/middleware/AccessMiddleware.php
+++ src/application/api/middleware/AccessMiddleware.php
@@ -13,10 +13,12 @@ class AccessMiddleware
      */
     public function handle($request, \Closure $next)
     {
-        /**
-         * @var Response
-         */
-        $response = $next($request);
+        if($request->isOptions()){
+            $response = Response::create();
+        }else{
+            $response = $next($request);
+        }
+        
         $response->header('Access-Control-Allow-Origin','*');
         $response->header('Access-Control-Allow-Methods','*');
         $response->header('Access-Control-Allow-Headers','x-requested-with,content-type,token');
