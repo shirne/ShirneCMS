d42b5a11c1d29ca25800208284e6485182065e5b
shirne <shirne@126.com>
2021-03-28 00:10:08 +0800

    fix

--- dbscript/updates/20210325.sql
+++ dbscript/updates/20210325.sql
@@ -3,7 +3,7 @@ add  `fields` TEXT COMMENT '开启的字段' after `props`,
 add  `template_dir` varchar(20) DEFAULT 0 COMMENT '独立模板目录' after `use_template`,
 add `channel_mode` tinyint(11) DEFAULT 0 COMMENT '频道模式' after `template_dir`,
 add `is_comment` tinyint(11) DEFAULT 0 COMMENT '是否开启评论' after `channel_mode`,
-add `is_images` tinyint(11) DEFAULT 0 COMMENT '是否有图集' after `is_single`,
+add `is_images` tinyint(11) DEFAULT 0 COMMENT '是否有图集' after `is_comment`,
 add `is_attachments` tinyint(11) DEFAULT 0 COMMENT '是否有附件' after `is_images`;
 
 alter TABLE `sa_article`
