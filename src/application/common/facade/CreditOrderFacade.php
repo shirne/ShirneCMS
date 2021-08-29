c8f3c70882f7c63b37fbd87a7aad756cd4ad3bab
shirne <shirne@126.com>
2021-08-29 09:31:04 +0800

    注解优化

--- src/application/common/facade/CreditOrderFacade.php
+++ src/application/common/facade/CreditOrderFacade.php
@@ -8,8 +8,8 @@ use app\common\core\SimpleFacade;
  * Class CreditOrderFacade
  * @package app\common\facade
  * @see \app\common\model\CreditOrderModel
- * @method bool makeOrder($member,$products,$address,$paycredit,$remark,$balance_pay=1) static 下单
- * @method string getError() static 获取错误信息
+ * @method static bool makeOrder($member,$products,$address,$paycredit,$remark,$balance_pay=1) 下单
+ * @method static string getError() 获取错误信息
  */
 class CreditOrderFacade extends SimpleFacade
 {
--- src/application/common/facade/GoodsCategoryFacade.php
+++ src/application/common/facade/GoodsCategoryFacade.php
@@ -9,13 +9,13 @@ use app\common\core\SimpleFacade;
  * Class GoodsCategoryFacade
  * @package app\common\facade
  * @see \app\common\model\GoodsCategoryModel
- * @method array getCategories($force=false) static 获取分类列表
- * @method array findCategory($idorname) static 查找分类
- * @method array getCategoryId($idorname) static 查找分类ID
- * @method array getCategoryTree($idorname) static 获取当前分类所在的层级树
- * @method array getTreedCategory($force=false) static 获取排序后的分类
- * @method array getSubCateIds($pid) static 获取下级分类id列表
- * @method void clearCache() static 清除缓存
+ * @method static array getCategories($force=false) 获取分类列表
+ * @method static array findCategory($idorname) 查找分类
+ * @method static array getCategoryId($idorname) 查找分类ID
+ * @method static array getCategoryTree($idorname) 获取当前分类所在的层级树
+ * @method static array getTreedCategory($force=false) 获取排序后的分类
+ * @method static array getSubCateIds($pid) 获取下级分类id列表
+ * @method static void clearCache() 清除缓存
  */
 class GoodsCategoryFacade extends SimpleFacade
 {
