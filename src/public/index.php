b3af526b2f700ba86021467d89df87dde7e8f2a0
shirne <shirne@126.com>
2024-06-12 19:56:21 +0800

    忽略array key 异常

--- src/public/index.php
+++ src/public/index.php
@@ -12,10 +12,19 @@
 // [ 应用入口文件 ]
 namespace think;
 
-define('DOC_ROOT',__DIR__);
+define('DOC_ROOT', __DIR__);
 
 // 加载基础文件
 require __DIR__ . '/../thinkphp/base.php';
+
+// 忽略 array key 异常
+set_error_handler(function (int $code, string $message, string $file = '', int $line = 0, ?array $context = []) {
+    if (strpos($message, 'Undefined array key') == 0) {
+        Log::error($code . ':' . $message . "\n$file [$line]");
+        return true;
+    }
+    return false;
+}, E_ALL);
 
 // 支持事先使用静态方法设置Request对象和Config对象
 
