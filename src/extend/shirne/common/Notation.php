c93d2e8fd71935867859188f8168c5b8eebf90c0
shirne <shirne@126.com>
2025-03-05 15:32:00 +0800

    fix notation

--- src/extend/shirne/common/Notation.php
+++ src/extend/shirne/common/Notation.php
@@ -3,10 +3,11 @@
 namespace shirne\common;
 
 
-class Notation {
+class Notation
+{
 
     //正则表达式，用于将表达式字符串，解析为单独的运算符和操作项
-    const PATTERN_EXP = '/((?:[a-zA-Z0-9_]+)|(?:[\(\)\+\-\*\/])){1}/';
+    const PATTERN_EXP = '/((?:[a-zA-Z0-9_\.]+)|(?:[\(\)\+\-\*\/])){1}/';
     const EXP_PRIORITIES = ['+' => 1, '-' => 1, '*' => 2, '/' => 2, "(" => 0, ")" => 0];
 
     /*
@@ -14,8 +15,9 @@ class Notation {
           $exp-普通表达式，例如 a+b*(c+d)
           $exp_values-表达式对应数据内容，例如 ['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4]
     */
-    public static function calculate($exp, $exp_values=[]) {
-        $exp_arr = self::parse_exp($exp);//将表达式字符串解析为列表
+    public static function calculate($exp, $exp_values = [])
+    {
+        $exp_arr = self::parse_exp($exp); //将表达式字符串解析为列表
         if (!is_array($exp_arr)) {
             return NULL;
         }
@@ -24,22 +26,24 @@ class Notation {
     }
 
     //将字符串中每个操作项和预算符都解析出来
-    protected static function parse_exp($exp) {
+    protected static function parse_exp($exp)
+    {
         $match = [];
         preg_match_all(self::PATTERN_EXP, $exp, $match);
         if ($match) {
             return $match[0];
-        }else {
+        } else {
             return NULL;
         }
     }
 
     //将中缀表达式转为后缀表达式
-    protected static function nifix2rpn($input_queue){
+    protected static function nifix2rpn($input_queue)
+    {
         $exp_stack = [];
         $output_queue = [];
-        foreach($input_queue as $input) {
-            if (in_array($input, array_keys(self::EXP_PRIORITIES))){
+        foreach ($input_queue as $input) {
+            if (in_array($input, array_keys(self::EXP_PRIORITIES))) {
                 if ($input == "(") {
                     array_push($exp_stack, $input);
                     continue;
@@ -52,29 +56,30 @@ class Notation {
                     }
                     continue;
                 }
-                foreach(array_reverse($exp_stack) as $exp) {
+                foreach (array_reverse($exp_stack) as $exp) {
                     if (self::EXP_PRIORITIES[$input] <= self::EXP_PRIORITIES[$exp]) {
                         array_pop($exp_stack);
                         array_push($output_queue, $exp);
-                    }else {
+                    } else {
                         break;
                     }
                 }
-                array_push($exp_stack ,$input);
-            }else {
+                array_push($exp_stack, $input);
+            } else {
                 array_push($output_queue, $input);
             }
         }
-        foreach(array_reverse($exp_stack) as $exp) {
+        foreach (array_reverse($exp_stack) as $exp) {
             array_push($output_queue, $exp);
         }
         return $output_queue;
     }
 
     //传入后缀表达式队列、各项对应值的数组，计算出结果
-    protected static function calculate_value($output_queue, $exp_values=[]) {
+    protected static function calculate_value($output_queue, $exp_values = [])
+    {
         $res_stack = [];
-        foreach($output_queue as $out) {
+        foreach ($output_queue as $out) {
             if (in_array($out, array_keys(self::EXP_PRIORITIES))) {
                 $a = array_pop($res_stack);
                 $b = array_pop($res_stack);
@@ -92,18 +97,17 @@ class Notation {
                         $res = $b / $a;
                         break;
                     default:
-                        continue;
+                        break;
                 }
-                array_push($res_stack, $res);
-            }else {
+                if (isset($res)) array_push($res_stack, $res);
+            } else {
                 if (is_numeric($out)) {
-                    array_push($res_stack, intval($out));
-                }else {
+                    array_push($res_stack, floatval($out));
+                } else {
                     array_push($res_stack, $exp_values[$out]);
                 }
             }
         }
         return count($res_stack) == 1 ? $res_stack[0] : NULL;
     }
-
 }
